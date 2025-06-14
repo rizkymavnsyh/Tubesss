8abbc0560a3190b850d8c510e194a97960d730b9
rizkymavnsyh <rizkymavnsyh@gmail.com>
2025-06-14 17:48:35 +0700

    Tubesss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-4xl font-bold text-blue-700">Selamat Datang</h1>
+        <p class="mt-4 text-lg text-gray-600">Pada Website...</p>
+    </div>
+@endsection
